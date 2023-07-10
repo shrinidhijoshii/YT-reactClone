0056cefbb5596b47149941285a5baa1dcacc77d7
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-10 22:45:03 +0530

    built sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,30 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>
-     sidebar
+    <div className="shadow-xl w-48 p-5">
+      <div className="m-3">
+        <h1 className="">Home</h1>
+        <h1 className="">Shorts</h1>
+        <h1 className="">Videos</h1>
+        <h1 className="">Live</h1>
+      </div>
+      <div className="m-3">
+        <h1 className="font-bold">Subscriptions</h1>
+        <ul>Striver</ul>
+        <ul>PowerfulJRE</ul>
+        <ul>Tanmay Bhat</ul>
+        <ul>T-series</ul>
+        <ul>PraveenMohan</ul>
+      </div>
+      <div className="m-3">
+        <h1 className="font-bold">Explore</h1>
+        <ul>Trending</ul>
+        <ul>Music</ul>
+        <ul>News</ul>
+        <ul>Sports</ul>
+      </div>
     </div>
-    
-  )
+  );
 }
 
 export default Sidebar
