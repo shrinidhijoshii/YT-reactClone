eefc30c24e5f53b5b3de87898bf632a60ba09119
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-12 23:51:54 +0530

    built more components on watch page

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyAXXswR90gvfIHnJCpf12LPgz2-XDQR4uA";
+export const GOOGLE_API_KEY = "AIzaSyAXXswR90gvfIHnJCpf12LPgz2-XDQR4uA";
 export const getYtVideosApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
 export const watchVidApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&id=${123}&key=${GOOGLE_API_KEY}`;
