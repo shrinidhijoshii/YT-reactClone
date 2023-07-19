0ed6afa45945213a4ace9122f8657b593af6b113
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-19 22:09:22 +0530

    added polling logic

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import LiveChatMessage from './LiveChatMessage';
 
 const LiveChat = () => {
+
+    useEffect(() => {
+        const timeInterval = setInterval(() => {
+            console.log("API polling")
+        }, 2000);
+
+        return () => {
+            clearInterval(timeInterval);
+        }
+
+    },[])
+
   return (
-    <div className='p-1 m-1 w-72 cursor-pointer shadow-lg h-[600px] bg-gray-100'>
-      LiveChat
+    <div className="p-1 m-1 w-72 cursor-pointer shadow-lg h-[600px] bg-gray-100">
+      <h1 className="text-center m-2">Live chat</h1>
+      <LiveChatMessage name="shrinidhi" message={"welcome🚀"} />
+      <LiveChatMessage name="shrinidhi" message={"welcome🚀"} />
+      <LiveChatMessage name="shrinidhi" message={"welcome🚀"} />
+      <LiveChatMessage name="shrinidhi" message={"welcome🚀"} />
+      <LiveChatMessage name="shrinidhi" message={"welcome🚀"} />
     </div>
   );
 }
---
+++ src/components/LiveChatMessage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LiveChatMessage = ({name, message}) => {
+  return (
+    <div className="flex shadow-md p-1">
+      <img
+        className="mx-1 h-6"
+        src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+      />
+      <p className="font-medium mx-1">{name}</p>
+      <span className="mx-1">{message}</span>
+    </div>
+  );
+}
+
+export default LiveChatMessage
