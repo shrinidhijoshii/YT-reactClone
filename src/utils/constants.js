55e5a1e6536abb6d1afc50b01500a60f9118e9c7
Shrinidhi Joshi <shrinidhijoshi99@gmail.com>
2024-07-15 21:49:30 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyAXXswR90gvfIHnJCpf12LPgz2-XDQR4uA";
+export const GOOGLE_API_KEY = "AIzaSyBMt1ovDvEY7YAvqa0rLP3X668Mi_WTweM";
 export const getYtVideosApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${GOOGLE_API_KEY}`;
 export const watchVidApi = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&id=${123}&key=${GOOGLE_API_KEY}`;
 export const searchApi =
